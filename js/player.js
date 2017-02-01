5d3bfc5f4675a9c1f1303eb2605967702bf20c9a
Valaandinos <valandinos@hotmail.com>
2017-02-01 06:01:10 +0100

    Minor bug correction

--- js/player.js
+++ js/player.js
@@ -1,28 +1,44 @@
+/**
+* Variables globales utilisées à peu près partout dans la suite du code
+*/
 var player = document.getElementById("player");
 var source = document.getElementById("source");
 var timer = document.getElementById("timer");
 var playlist = document.getElementById("playlist");
 var playLoop;
-var actualSong = 2;
+var actualSong = 2; //défini de base à 2 à cause du scandiff de PHP qui ôte les deux premiers éléments ("." et "..")
+var diviseur = 1;
 
+/**
+* Function de chargement de la chanson dans la balise "audio"
+*/
 function load(){
     source.src = dir + "/" + songList[actualSong];
     player.load();
     window.clearInterval(playLoop);
 }
 
+/**
+* Fonction de lecture
+*/
 function play(){
     showCurrentSong();
     player.play();
     playLoop = setInterval(runningLoop, 1000);
-
 }
 
+/**
+* Fonction de mise en pause de la lecture
+*/
 function pause(){
     player.pause();
     window.clearInterval(playLoop);
 }
 
+/**
+* Fonction d'arrêt complet de la lecture
+* Vide la balise audio et remets tous les compteurs à 0
+*/
 function stop(){
     player.pause();
     player.currentTime = 0;
@@ -30,10 +46,15 @@ function stop(){
     timer.innerHTML = "";
     removeCurrentSong();
     actualSong = 2;
+    resetDiviseur();
 }
 
+/**
+* Fonction de changement de chanson, utilisée par les boutons "précédent" et "suivant"
+*/
 function changeSong(e){
     removeCurrentSong();
+    resetDiviseur();
     if(e == "1" && actualSong <= Object.keys(songList).length){
         actualSong++;
         load();
@@ -49,22 +70,42 @@ function changeSong(e){
     }
 }
 
+/**
+* Boucle de lecture
+* Permet l'affichage du temps écoulé lors de lecture
+* Permet le passage automatique à la chanson suivante une fois la lecture de celle en cours terminée
+* L'affichage correct de la durée écoulée est un peu capricieux au moment du passage d'une minute à l'autre,
+* alors il a fallu bricoler un peu pour que ça fonctionne. C'est pas propre du tout, mais ça tourne.
+*/
 function runningLoop(){
-    var duration = " / " + Math.floor(player.duration/60) + ":" + Math.round(player.duration%60);
-    var x = Math.round(player.currentTime%60);
-    var y;
-    if(x <= 9){
-      var y = ":0";
+    var totalDuration = " / " + Math.floor(player.duration/60) + ":" + Math.round(player.duration%60);
+    var currentSec;
+    var currentMinute;
+    var dot;
+    if (player.currentTime/diviseur > 59 && player.currentTime/diviseur < 60) {
+        currentMinute = Math.round(player.currentTime/60);
+        currentSec = 0;
+        diviseur++;
     }
     else {
-      var y = ":"
+        currentMinute = Math.floor(player.currentTime/60);
+        currentSec = Math.round(player.currentTime%60);
     }
-    timer.innerHTML = Math.floor(player.currentTime/60) + y + x + duration;
+    if(currentSec <= 9){
+      var dot = ":0";
+    }
+    else {
+      var dot = ":"
+    }
+    timer.innerHTML = currentMinute + dot + currentSec + totalDuration;
     if(player.ended == true){
         changeSong(1);
     };
 }
 
+/**
+* Fonction de génération de la liste de lecture
+*/
 function showPlaylist(){
     var tabLength = Object.keys(songList).length + 2;
     var list = "<dl>";
@@ -75,22 +116,39 @@ function showPlaylist(){
     playlist.innerHTML = list;
 }
 
+/**
+* Fonction de mise en évidence de la chanson actuellement en cours de lecture
+*/
 function showCurrentSong(){
     document.getElementById(actualSong).className = "currentlyPlaying";
 }
 
+/**
+* Fonction servant à enlever la mise en évidence de la chanson actuellement en cours de lecture
+*/
 function removeCurrentSong(){
     document.getElementById(actualSong).removeAttribute("class");
 }
 
+/**
+* Fonction servant à changer la chanson en cours de lecture par celle sur laquelle on a cliqué dans la liste de lecture
+*/
 function switchCurrentSong(x){
     removeCurrentSong();
+    resetDiviseur();
     var current = x.getAttribute("id");
     actualSong = current;
     load();
     play();
 }
 
+
+function resetDiviseur(){
+    diviseur = 1;
+}
+/**
+*
+*/
 function calculateTimePassed(){
     var currentTime = player.currentTime;
     var totalTime = player.duration;
